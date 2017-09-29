3f23f638152083a6846ee6f1f651502361019b06
darren <764190362@qq.com>
2017-09-29 13:36:18 +0800

    几个中间件以及用到了服务

---
+++ Infrastructure/BrowserTypeMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreConfiguration.Infrastructure
+{
+    /// <summary>
+    /// 改变请求的中间件
+    /// </summary>
+    public class BrowserTypeMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public BrowserTypeMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items["EdgeBrowser"] = httpContext.Request.Headers["User-Agent"].Any(v => v.ToLower().Contains("edge"));
+            await nextDelegate.Invoke(httpContext);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreConfiguration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel() //使用Kestrel服务器
+                .UseContentRoot(Directory.GetCurrentDirectory()) //读出根目录下的配置文件和其它静态文件，比如image，css,js等
+                .UseIISIntegration() //匹配IIS和IISExpress
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
